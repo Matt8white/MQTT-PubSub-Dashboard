0d813ec1802047dae970308404ca3189c45e03ac
Matt8white <matt93white@gmail.com>
2017-05-04 12:43:38 +0200

    First try with a simple pub-sub w/ hello world successfully tested

--- publisher.py
+++ publisher.py
@@ -3,6 +3,6 @@ import paho.mqtt.client as mqtt
 # This is the Publisher
 
 client = mqtt.Client()
-client.connect("localhost",1883,60)
-client.publish("topic/test", "Hello world!");
-client.disconnect();
+client.connect("localhost", 1883, 60)
+client.publish("topic/test", "Hello world!")
+client.disconnect()
