d5fea4441474a7ed3480227a194941e4ef80a6fa
Matt8white <matt93white@gmail.com>
2017-05-07 13:24:37 +0200

    First working implementation of publisher-subscriber communication over MQTT

--- publisher.py
+++ publisher.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import time
 import psutil
 # print psutil.cpu_times()
 
@@ -6,5 +7,12 @@ import psutil
 
 client = mqtt.Client()
 client.connect("localhost", 1883, 60)
-client.publish("topic/test", "Hello world!")
+start_time = int(time.time())
+while int(time.time()) - start_time < 60:
+    client.publish("topic/cpu", str(psutil.cpu_percent()) + ' ' + str(int(time.time())))
+    client.publish("topic/memory", str(psutil.virtual_memory()[2]) + ' ' + str(int(time.time())))
+    time.sleep(1)
+# client.publish("topic/memory", int(time.time()))
+# client.publish("topic/disk_usage", int(time.time()))
+client.publish("topic/status", "Quit")
 client.disconnect()
--- subscriber.py
+++ subscriber.py
@@ -1,23 +1,41 @@
 import paho.mqtt.client as mqtt
+import platform
+import socket
+import time
+
+
+CARBON_SERVER = '127.0.0.1'
+CARBON_PORT = 2003
+DELAY = 1  # secs
+
+
+# Send messages to graphite
+def send_msg(message):
+    print('sending to graphite message:\n%s' % message)
+    sock = socket.socket()
+    sock.connect((CARBON_SERVER, CARBON_PORT))
+    sock.sendall(message)
+    sock.close()
 
 
 # This is the Subscriber
-
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-    client.subscribe("topic/test")
+    client.subscribe([("topic/cpu", 0),
+                      ("topic/memory", 0),
+                      ("topic/disk_usage", 0),
+                      ("topic/status", 0)])
 
 
 def on_message(client, userdata, msg):
-    if msg.payload.decode() == "Hello world!":
-        print("Yes!")
+    if msg.topic.decode() == "topic/status" and msg.payload.decode() == "Quit":
         client.disconnect()
+    message = msg.topic.decode().replace('/', '.') + ' ' + msg.payload.decode() + '\n'
+    send_msg(message)
 
 
 client = mqtt.Client()
 client.connect("localhost", 1883, 60)
-
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.loop_forever()
