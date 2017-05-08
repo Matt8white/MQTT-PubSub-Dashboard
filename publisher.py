47517c3af1a8ab616cf1729cdc4d6adfb732156a
Matt8white <matt93white@gmail.com>
2017-05-08 15:41:01 +0200

    Added some features and commented all code

--- publisher.py
+++ publisher.py
@@ -1,15 +1,36 @@
 import paho.mqtt.client as mqtt
+import numpy as np
+import argparse
 import time
 import psutil
 
+# Input parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--mqtt_address', type=str, default="localhost")
+parser.add_argument('--mqtt_port', type=int, default=1883)
+parser.add_argument('--run_time', type=int, default=0)
+args = parser.parse_args()
+
+mqtt_address = args.mqtt_address
+mqtt_port = args.mqtt_port
+run_time = args.run_time
+# If no run_time is defined the script will run forever
+if run_time == 0:
+    run_time = np.inf
+    
+# Connecting to MQTT message broker
 client = mqtt.Client()
-client.connect("localhost", 1883, 60)
+client.connect(mqtt_address, mqtt_port, 60)
+
 start_time = int(time.time())
-while int(time.time()) - start_time < 300:  # TODO put this as parameter
+while int(time.time()) - start_time < run_time:
     client.publish("topic/cpu", str(psutil.cpu_percent()) + ' ' + str(int(time.time())))
     client.publish("topic/memory", str(psutil.virtual_memory()[2]) + ' ' + str(int(time.time())))
-    # client.publish("topic/memory", int(time.time()))
-    # client.publish("topic/disk_usage", int(time.time()))
+    client.publish("topic/disk_usage", str(psutil.disk_usage('/')[3]) + ' ' + str(int(time.time())))
+    client.publish("topic/disk_reads", str(psutil.disk_io_counters()[0]) + ' ' + str(int(time.time())))
+    client.publish("topic/disk_writes", str(psutil.disk_io_counters()[1]) + ' ' + str(int(time.time())))
     time.sleep(1)
+
+# Shut all the listening subscribers and disconnects itself
 client.publish("topic/status", "Quit")
 client.disconnect()
--- subscriber.py
+++ subscriber.py
@@ -1,29 +1,78 @@
 import paho.mqtt.client as mqtt
+import argparse
 import socket
 
+# Input parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--mqtt_address', type=str, default="localhost")
+parser.add_argument('--mqtt_port', type=int, default=1883)
+parser.add_argument('--carbon_address', type=str, default="127.0.0.1")
+parser.add_argument('--carbon_port', type=int, default=2003)
+args = parser.parse_args()
 
-CARBON_SERVER = '127.0.0.1'
-CARBON_PORT = 2003
+mqtt_address = args.mqtt_address
+mqtt_port = args.mqtt_port
+carbon_address = args.carbon_address
+carbon_port = args.carbon_port
 
 
-# Send messages to graphite
 def send_msg(message):
+    """
+    Sends a properly formatted message to graphite DB
+    
+    Parameters
+    ----------
+    message: The message to be sent
+    
+    Returns
+    -------
+    Nothing
+    """
     print('sending to graphite message:\n%s' % message)
     sock = socket.socket()
-    sock.connect((CARBON_SERVER, CARBON_PORT))
+    sock.connect((carbon_address, carbon_port))
     sock.sendall(message)
     sock.close()
 
 
 def on_connect(client, userdata, flags, rc):
+    """
+    Handles the connection to the message broker
+
+    Parameters
+    ----------
+    client: The client who wants to connect to the broker
+    userdata: The private user data
+    flags: Response flags sent by the broker
+    rc: The connection result
+
+    Returns
+    -------
+    Nothing
+    """
     print("Connected with result code " + str(rc))
     client.subscribe([("topic/cpu", 0),
                       ("topic/memory", 0),
                       ("topic/disk_usage", 0),
+                      ("topic/disk_reads", 0),
+                      ("topic/disk_writes", 0),
                       ("topic/status", 0)])
 
 
 def on_message(client, userdata, msg):
+    """
+    Handles the received message
+    
+    Parameters
+    ----------
+    client: The client who has received the message
+    userdata: The private user data
+    msg: Content of the message, divided in topic and payload
+    
+    Returns
+    -------
+    Nothing
+    """
     if msg.topic.decode() == "topic/status" and msg.payload.decode() == "Quit":
         client.disconnect()
     else:
@@ -31,8 +80,13 @@ def on_message(client, userdata, msg):
         send_msg(message)
 
 
+# Connecting to MQTT message broker
 client = mqtt.Client()
-client.connect("localhost", 1883, 60)
+client.connect(mqtt_address, mqtt_port, 60)
+
+# Assigning actions (functions) to events
 client.on_connect = on_connect
 client.on_message = on_message
+
+# Loops until a "client.disconnect() function is called
 client.loop_forever()
