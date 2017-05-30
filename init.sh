bd67b0a8ec9c3e8fcbaf9dc1cb2d0418fb8934a6
Matt8white <matt93white@gmail.com>
2017-05-30 16:18:06 +0200

    Removing init

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python /Users/mattwhite/anaconda/bin/carbon-cache.py start
-export PYTHONPATH="/Users/mattwhite/anaconda/webapp"
-python run-graphite-devel-server.py /Users/mattwhite/anaconda/webapp/graphite
-grafana-server -homepath /usr/local/share/grafana/
